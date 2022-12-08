256b2409d34b8fc2a1310b53fa2a00c7b5920b24
David Seow <david.seow@gmail.com>
2022-12-08 23:17:45 +0000

    Refactor to enable script for https

--- scripts/open_git_branch_in_browser.sh
+++ scripts/open_git_branch_in_browser.sh
@@ -1,15 +1,27 @@
 #! /bin/bash
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-REPOSITORY_URL=$(git remote -v | grep .com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e 's/:/\//' -e 's/git@/http:\/\//' | sed 's/.git$//g')
 
-# if Gitlab
-if [[ "$REPOSITORY_URL" == *"gitlab"* ]];
-then
-  REPOSITORY_BRANCH_URL="$REPOSITORY_URL/-/tree/$BRANCH_NAME"
-# else (Github)
+RAW_REPOSITORY_URL=$(git remote -v | grep .com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1)
+if [[ $RAW_REPOSITORY_URL == "git@"* ]]; then
+  REPOSITORY_URL=$(echo $RAW_REPOSITORY_URL | sed -e 's/:/\//' -e 's/git@/https:\/\//' | sed 's/.git$//g')
 else
-  REPOSITORY_BRANCH_URL="$REPOSITORY_URL/tree/$BRANCH_NAME"
+  REPOSITORY_URL=$(echo $RAW_REPOSITORY_URL | sed 's/.git$//g')
 fi
 
-open -a "Google Chrome" $REPOSITORY_BRANCH_URL
+URL_PATH=""
+
+# ci
+if [[ "$1" == "ci" ]] && [[ "$REPOSITORY_URL" == *"gitlab"* ]]; then
+    URL_PATH="-/pipelines"
+elif [[ "$1" == "ci" ]] && ! [[ "$REPOSITORY_URL" == *"gitlab"* ]]; then
+    URL_PATH="actions"
+
+# branch
+elif [[ -z "$1" ]] && [[ "$REPOSITORY_URL" == *"gitlab"* ]]; then
+  URL_PATH="-/tree/$BRANCH_NAME"
+elif [[ -z "$1" ]] && ! [[ "$REPOSITORY_URL" == *"gitlab"* ]]; then
+  URL_PATH="tree/$BRANCH_NAME"
+fi
+
+open -a "Google Chrome" "$REPOSITORY_URL/$URL_PATH"
